84e71385bb5cb00fc95a7a646633142d00578e86
anybodywise <anybodywise@gmail.com>
2016-02-23 16:54:11 +0100

    Add Task2

---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var weather_forecast = new[]
+            {
+                new Weather("Austria", "Vienna", "Cloudy", 60.8, Unit.Fahrenheit),
+                new Weather("Ukraine", "Kyiv", "Sunny", 13.2, Unit.Celsius),
+                new Weather("Croatia", "Split", "Sunny", 20.1, Unit.Celsius),
+            };
+            foreach (var w in weather_forecast)
+            {
+                Console.WriteLine("{0}/{1}\n{2}\n{3:0.00}°\n", w.Country, w.City, w.Clouds, w.GetTemperature(Unit.Celsius));
+            }
+        }
+    }
+}

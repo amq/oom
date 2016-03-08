7ec3e66374b4257f70799115449495aff5edc256
anybodywise ‪ <anybodywise >
2016-03-08 19:32:11 +0100

    Add Task4

---
+++ tasks/Task4/Task4/Weather.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public interface IWeather
+    {
+        /// <summary>
+        /// Gets the clouds.
+        /// </summary>
+        string Clouds { get; }
+
+        Unit Unit { get; set; }
+
+        /// <summary>
+        /// Gets the temperature in a given unit.
+        /// </summary>
+        double GetTemperature(Unit unit);
+
+        /// <summary>
+        /// Updates the temperature.
+        /// </summary>
+        /// <param name="newTemperature">Temperature.</param>
+        /// <param name="newUnit">Unit.</param>
+        void UpdateTemperature(Temperature newTemperature);
+    }
+}
---
+++ tasks/Task4/Tests/ForecastTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using Task4;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ForecastTest
+    {
+        [Test]
+        public void CanCreateForecast()
+        {
+            var x = new Forecast("Austria", "Vienna", "Cloudy", 60.8, Unit.Fahrenheit);
+
+            Assert.IsTrue(x.Country == "Austria");
+            Assert.IsTrue(x.City == "Vienna");
+            Assert.IsTrue(x.Clouds == "Cloudy");
+            Assert.IsTrue(x.Temperature.Number == 60.8);
+            Assert.IsTrue(x.Temperature.Unit == Unit.Fahrenheit);
+        }
+        [Test]
+        public void CanCreateHistory()
+        {
+            var x = new History("Czech Republic", "Prague", DateTime.Now, "Sunny", 10.5, Unit.Celsius);
+
+            Assert.IsTrue(x.Country == "Czech Republic");
+            Assert.IsTrue(x.City == "Prague");
+            Assert.IsTrue(x.Clouds == "Sunny");
+            Assert.IsTrue(x.Temperature.Number == 10.5);
+            Assert.IsTrue(x.Temperature.Unit == Unit.Celsius);
+        }
+    }
+}
+
